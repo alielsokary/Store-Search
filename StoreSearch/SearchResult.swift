d4b0bab8bf5745662e0121df18212055b3b6e1f1
Ali Elsokary <ali.elsokary8@gmail.com>
2018-02-23 01:59:51 +0200

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Ali on 2/23/18.
+//  Copyright © 2018 mag. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
