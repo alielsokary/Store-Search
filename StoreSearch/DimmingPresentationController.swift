f16080cb53fbe2bad49a3f1fbfe47bfdb2ebd76e
Ali Elsokary <ali.elsokary8@gmail.com>
2018-03-10 13:04:44 +0200

    - Add store item data to popupView.

---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,14 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Ali on 3/6/18.
+//  Copyright © 2018 mag. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    } }
